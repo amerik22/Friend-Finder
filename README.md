e4b5e8fb0dd3903a252ccbc8a2347cb3cab5533b
Andrea <andrea.erikson@gmail.com>
2019-01-27 22:22:52 -0700

    updated README, survey and friends data

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Friend-Finder
+The "FriendFinder" application -- is basically a dating app but for friends. Meet your match! The site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match. 
+
+# Link to Project
+
+
+# Tech Used
+HTML
+Bootstrap Framework
+Javascript
+JQuery
+Node.js
+Express Package
+Path Package 
--- app/data/friends.js
+++ app/data/friends.js
@@ -3,10 +3,16 @@ var friends = [
         name: "Sully",
         photo: "https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg",
         scores: [
-            "4",
-            "3",
-            "2",
-            "4"
+            4,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            2,
+            1,
+            5
         ]
 
     },
@@ -14,30 +20,48 @@ var friends = [
         name: "Bradley",
         photo: "https://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/images/2013/TRANSFORMATIONS/2003-bradley-cooper-400_0.jpg?itok=LNVA8jd9",
         scores: [
-            "2",
-            "3",
-            "3",
-            "4"
+            2,
+            3,
+            3,
+            4,
+            1,
+            5,
+            2,
+            4,
+            5,
+            2
         ]
     },
     {
         name: "Cleo",
         photo: "https://www.telegraph.co.uk/content/dam/pets/2017/01/06/1-JS117202740-yana-two-face-cat-news_trans_NvBQzQNjv4BqJNqHJA5DVIMqgv_1zKR2kxRY9bnFVTp4QZlQjJfe6H0.jpg?imwidth=450",
         scores: [
-            "1",
-            "5",
-            "2",
-            "2"
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
         ]
     },
     {
         name: "Mandy",
         photo: "https://cdn.cliqueinc.com/cache/posts/242380/mandy-moore-confidence-242380-1510871479965-main.700x0c.jpg",
         scores: [
-            "5",
-            "3",
-            "4",
-            "5"
+            5,
+            3,
+            2,
+            1,
+            1,
+            4,
+            2,
+            3,
+            1,
+            5
         ]
     }
 ]
